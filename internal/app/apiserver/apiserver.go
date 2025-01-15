feb0da1f02672bc3db373a6278495c1e8f72b1ba
Nikita Mamaev <nikratosdev@gmail.com>
2025-01-15 14:48:48 +0200

    feat: added router and handle Hello, added internal test

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,16 +1,23 @@
 package apiserver
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+)
 
 type APIServer struct {
 	config *Config
 	logger *logrus.Logger
+	router *mux.Router
 }
 
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
 		logger: logrus.New(),
+		router: mux.NewRouter(),
 	}
 }
 
@@ -19,9 +26,11 @@ func (s *APIServer) Start() error {
 		return err
 	}
 
+	s.configureRouter()
+
 	s.logger.Info("starting API server")
 
-	return nil
+	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
 
 func (s *APIServer) configureLogger() error {
@@ -31,5 +40,16 @@ func (s *APIServer) configureLogger() error {
 	}
 
 	s.logger.SetLevel(level)
+
 	return nil
+}
+
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello")
+	}
 }
